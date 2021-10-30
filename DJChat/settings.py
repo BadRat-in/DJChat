505c745b4e9991a408dfb715c26a532e8866f317
BadRat-in <67710516+BadRat-in@users.noreply.github.com>
2021-10-30 21:08:36 +0530

    Update settings.py

--- DJChat/settings.py
+++ DJChat/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q^kk2!q4&6gs#tdo9r(0x#ceo+q0^wuf318_e_8#_cd#&af0!^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS=['127.0.0.1', 'localhost']
+ALLOWED_HOSTS=['127.0.0.1', 'localhost', 'chatdjseguro.herokuapp.com']
 
 # Application definition
 
