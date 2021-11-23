c7bce7a3553274e5f85306928199890b372188cf
BadRat-in <67710516+BadRat-in@users.noreply.github.com>
2021-11-23 22:43:36 +0530

    Update timeline.js

--- static/js/timeline.js
+++ static/js/timeline.js
@@ -136,11 +136,11 @@ function SendtoChat(str) {
 
 
 function getMessage() {
+    msg_box.innerHTML = "";
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
             var msg = this.responseText;
-            msg_box.innerHTML = "";
             showmsg(msg);
             cmsg();
         }
